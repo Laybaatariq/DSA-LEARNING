ce1d0c644e244f8a9ccfa070d4d9b7e825509623
LAIBA TARIQ <135421663+Laybaatariq@users.noreply.github.com>
2026-08-28 15:32:42 +0000

    Added Successfully

---
+++ 01-Arrays/reverseArray.py
@@ -0,0 +1,14 @@
+
+# Reverse An Array by using two Pointers
+
+arr = [1,2,3,4,5]
+
+i = 0
+j = len(arr) -1
+
+while i<j:
+    arr[i],arr[j] = arr[j], arr[i]
+    i+=1
+    j-=1
+
+print("Reversed Array is :", arr)
