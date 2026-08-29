d515118c0d25bc574657da56cf2423cdbafeafcd
LAIBA TARIQ <135421663+Laybaatariq@users.noreply.github.com>
2026-08-29 14:02:43 +0000

    Added Successfully

---
+++ 01-Arrays/is_sorted.py
@@ -0,0 +1,13 @@
+
+# array is sorted or not
+
+arr = [1,2,3,4,5]
+
+is_sorted = True
+
+for i in range(len(arr) - 1):
+    if arr[i]>arr[i+1]:
+        is_sorted = False
+        break
+
+print("Array is sorted:", is_sorted)
